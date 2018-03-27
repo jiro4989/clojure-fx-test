927b3297a41bee890308a4d043fb7a25663568df
jiro4989 <jiroron666@gmail.com>
2018-03-28 06:39:50 +0900

    テストコード追加

--- src/jiro/app/image.clj
+++ src/jiro/app/image.clj
@@ -3,6 +3,13 @@
            [java.awt RenderingHints]
            [java.awt.geom AffineTransform]))
 
+;; 元の画像と同じ幅の空の画像を返す
+(defn new-image [^BufferedImage image]
+  (BufferedImage. 
+    (.getWidth image) 
+    (.getHeight image) 
+    BufferedImage/TYPE_INT_ARGB))
+
 ;; trim - 画像を指定の座標から指定の幅だけ切り出した新しい画像として返却する
 (defn trim [^BufferedImage image
             v]
@@ -12,19 +19,19 @@
 ;; scale - 画像を指定の拡大率で拡縮した新しい画像を返却する
 (defn scale [^BufferedImage image
              double rate]
-  (let [w (-> image .getWidth int)
-        h (-> image .getHeight int)]
-    (let [ni (BufferedImage. w h BufferedImage/TYPE_INT_ARGB)]
-      (doto (.createGraphics ni)
-        (.setRenderingHint RenderingHints/KEY_INTERPOLATION RenderingHints/VALUE_INTERPOLATION_BICUBIC)
-        (.scale rate rate)
-        (.drawImage image 0 0 nil)
-        .dispose)
-      ni)))
+  (let [ni (new-image image)]
+    (doto (.createGraphics ni)
+      (.setRenderingHint RenderingHints/KEY_INTERPOLATION RenderingHints/VALUE_INTERPOLATION_BICUBIC)
+      (.scale rate rate)
+      (.drawImage image 0 0 nil)
+      .dispose)
+    ni))
 
 ;; flip - 画像を左右反転した新しい画像を返却する
 (defn flip [^BufferedImage image]
-  (.. (doto (AffineTransform/getScaleInstance -1 1)
+  (let [ni (new-image image)]
+    (.. (doto (AffineTransform/getScaleInstance -1 1)
         (.translate (.getWidth image) 0)
         (AffineTransformOp. AffineTransformOp/TYPE_BICUBIC))
-      (filter image nil)))
+      (filter image ni))
+    ni))
---
+++ test/tkfm/image_test.clj
@@ -0,0 +1,27 @@
+(ns tkfm.image-test
+  (:require [clojure.test :refer :all]
+            [clojure.string :as str]
+            [clojure.java.io :as io]
+            [jiro.app.image :refer :all])
+  (:import [javax.imageio ImageIO]))
+
+(deftest trim-test
+  (testing "画像のトリミングのテスト"
+    (-> (ImageIO/read "./data/in/test01.png")
+        (trim {:x 0 :y 0 :width 144 :height 144})
+        (ImageIO/write "PNG" (io/file "./data/out/trim-test-result.png")))))
+
+(deftest scale-test
+  (testing "画像の縮小のテスト"
+    (-> (ImageIO/read (io/file  "./data/in/test01.png"))
+        (scale 0.5)
+        (ImageIO/write "PNG" (io/file "./data/out/scale-up-test-result.png")))
+    (-> (ImageIO/read "./data/in/test01.png")
+        (scale 1.5)
+        (ImageIO/write "PNG" (io/file "./data/out/scale-down-test-result.png")))))
+
+(deftest flip-test
+  (testing "画像の左右反転のテスト"
+    (-> (ImageIO/read (io/file  "./data/in/test01.png"))
+        flip
+        (ImageIO/write "PNG" (io/file "./data/out/flip-test-result.png")))))
